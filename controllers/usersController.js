c77615aac20f03a0771c37c7b8a15b8cc49a026c
konpits <31943872+konpits@users.noreply.github.com>
2018-07-17 17:26:49 +1000

    add error handling at usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,41 +2,65 @@ const User = require('../models/User')
 
 module.exports = {
     index: async (req, res, next) => {
+        try {
         const users = await User.find()
         res.status(200).json(users)
+        } catch(err) {
+            next(err)
+        }
     },
 
     registerUser: async (req, res, next) => {
+        try {
         const userOptions = req.body
         const user =  await User.register(userOptions)
         res.status(200).json(user)
+        } catch(err) {
+            next(err)
+        }
     },
 
     getUser: async (req, res, next) => {
+        try {
         const { userId } = req.params
         const user =  await User.findById(userId)
         res.status(200).json(user)
+        } catch(err) {
+            next(err)
+        }
     },
 
     replaceUser: async (req, res, next) => {
+        try {
         const { userId } = req.params
         const newUser = req.body
         const result =  await User.findByIdAndUpdate(userId, newUser, {new: true})
         res.status(200).json(result)
+        } catch(err) {
+            next(err)
+        }
     },
 
     updateUser: async (req, res, next) => {
+        try {
         const { userId } = req.params
         const newUser = req.body
         const result =  await User.findByIdAndUpdate(userId, newUser, {new: true})
         res.status(200).json(result)
+        } catch(err) {
+            next(err)
+        }
     },
 
     deleteUser: async (req, res, next) => {
+        try {
         const { userId } = req.params
         const user = await User.findById(userId)
         const result =  await user.remove()
         res.status(200).json(result)
+        } catch(err) {
+            next(err)
+        }
     }
-
+    
 }
