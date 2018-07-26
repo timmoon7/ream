b1e21ff946d10b3f7831c496de522c0e520b976b
Tim Moon <tim.moon7@gmail.com>
2018-07-26 10:34:48 +1000

    Add bcryptjs package and uninstalled bcrypt because of now server deployment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const interviewRouter = require('./routes/interviewRouter')
 const PORT = 3000
 app.use(express.json())
 app.use(cors({
-   origin: '*',
+   origin: 'https://ream.now.sh',
    credentials: true 
 }))
 app.use(cookieParser())
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('../controllers/connectdb')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const {Schema} = mongoose
 
 
